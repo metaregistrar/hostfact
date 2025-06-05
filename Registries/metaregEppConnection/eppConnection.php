16d80f2817e3759009cc554ff7b0ab7330a351ff
Ewout de Graaf <ewout@metaregistrar.com>
2025-06-05 14:17:42 +0200

    remove file

--- Registries/metaregEppConnection/eppConnection.php
+++ Registries/metaregEppConnection/eppConnection.php
@@ -9,14 +9,14 @@ class metaregEppConnection extends eppConnection {
 
         // Default server configuration stuff - this varies per connected registry
         // Check the greeting of the server to see which of these values you need to add
-        $this->enableDnssec();
         $this->enableRgp();
+        $this->enableDnssec();
         parent::useExtension('polldata-1.0');
-        parent::useExtension('command-ext-1.0');
         parent::useExtension('ext-1.0');
         parent::useExtension('dns-ext-1.0');
         parent::useExtension('ssl-1.0');
-        //parent::enableLaunchphase('claims');
+        parent::useExtension('command-ext-1.0');
+
     }
 
 }
