82bcc9cc9ff7c56e40f739807ef83af532285fdc
Ewout de Graaf <ewout@metaregistrar.com>
2025-06-05 13:19:45 +0200

    Updated version and registrar information

--- version.php
+++ version.php
@@ -1,15 +1,15 @@
 <?php
 $version['name']            = "Metaregistrar";
-$version['api_version']     = "1.2";
+$version['api_version']     = "6.x";
 $version['date']            = "2025-06-01"; // Last modification date
-$version['wefact_version']  = "3.6"; // Version released for WeFact
+$version['version']         = "1.2"; // Version released for WeFact
 $version['autorenew']       = true; // AutoRenew is default?  true | false
 $version['handle_support']  = true; // Handles are supported? true | false
 $version['cancel_direct']   = true; // Possible to terminate domains immediately?  true | false
 $version['cancel_expire']   = true; // Possible to stop auto-renew for domains? true | false
 
 // Information for customer (will be showed at registrar-show-page)
-$version['dev_logo']		= 'https://metaregistrar.com/wp-content/uploads/2021/02/logo-footer.png'; // URL to your logo
+$version['dev_logo']		= 'https://metaregistrar.com/logo.png'; // URL to your logo
 $version['dev_author']		= 'Metaregistrar BV'; // Your companyname
 $version['dev_website']		= 'https://www.metaregistrar.com'; // URL website
 $version['dev_email']		= 'support@metaregistrar.com'; // Your e-mailaddress for support questions
